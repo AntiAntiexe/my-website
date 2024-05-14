3a4902a682b2fe369bf22379fb8af859debfd949
AntiAntiexe <117411246+AntiAntiexe@users.noreply.github.com>
2024-05-15 09:02:36 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-console.log('Hello this is JAVA!')
+console.log('Hello this is JAVA!');
+let name = prompt('Hello and welcome to my website! What is your full name?', 'John Smith')
+
+alert(`Hello, ${name}`)
